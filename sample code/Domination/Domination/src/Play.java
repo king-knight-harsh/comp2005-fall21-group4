a258dccde00b8941346c6e7547af6576e46e16d8
Harsh Sharma <hsharma@mun.ca>
2021-11-21 20:18:39 -0330

    fixed the folder

--- sample code/Domination/Domination/src/Play.java
+++
@@ -1,8 +0,0 @@
-
-public class Play {
-
-	public static void main(String[] args) {
-		GameDisplay newGame = new GameDisplay();
-	}
-
-}
--- sample code/Domination/Domination/src/StackForGame.java
+++
@@ -1,9 +0,0 @@
-import java.util.Stack;
-
-public class StackForGame {
-	StackForGame(String piece){
-
-	Stack<String> stack = new Stack<String>();
-		
-	}
-}
