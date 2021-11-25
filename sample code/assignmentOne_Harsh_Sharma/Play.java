2ea87ec13277d881c0411f32ad7829ed1c080c4a
Harsh Sharma <hsharma@mun.ca>
2021-11-25 13:10:18 -0330

    assignment 1 for documentation reference

---
+++ sample code/assignmentOne_Harsh_Sharma/Play.java
@@ -0,0 +1,18 @@
+/*
+ *  Simple GUI game of treasure hunt using swing.
+ *  Here the GameDisplay class extends JPanel and
+ *  holds its coordinates in the grid as attributes.
+ *  @author Harsh Sharma -201961844
+ *  @version 09/23/2021 - 2.30 p.m.
+ */
+
+public class Play {
+	//Main method for the running the code
+	public static void main(String[] args) {
+		
+		//Initializing a local variable of GameDisplay
+		GameDisplay newGame = new GameDisplay(5,5);
+		
+	}
+
+}
